d8e6a7e3d2f849b45951c40ff229a083d3082e5b
Akshat Jain <jainakshat1900@gmail.com>
2024-07-08 17:43:06 +0530

    updated

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const colors = require("colors");
-const moragan = require("morgan");
+const morgan = require("morgan");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const path = require("path");
@@ -16,7 +16,7 @@ const app = express();
 
 //middlewares
 app.use(express.json());
-app.use(moragan("dev"));
+app.use(morgan("dev"));
 
 //routes
 app.use("/api/v1/user", require("./routes/userRoutes"));
@@ -35,7 +35,7 @@ const port = process.env.PORT || 3001;
 //listen port
 app.listen(port, () => {
   console.log(
-    `Server Running in ${process.env.NODE_MODE} Mode on port ${process.env.PORT}`
+    `Server Running in ${process.env.NODE_MODE || 'development'} Mode on port ${port}`
       .bgCyan.white
   );
 });
